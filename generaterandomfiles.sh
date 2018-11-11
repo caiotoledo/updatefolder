3d2bc847bac8d0cab73356c303e57f5ef0116298
Caio Toledo <caiovinicius.toledo@altran.com>
2018-11-11 00:42:54 +0000

    Add test scripts and config file for Travis CI
    Signed-off-by: Caio Toledo <caiovinicius.toledo@altran.com>

---
+++ generaterandomfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+generatefile(){
+	< /dev/urandom tr -dc "\t\n [:alnum:]" | head -c1000 >> $1
+}
+
+mkdir -p folders/testfolder1
+mkdir -p folders/testfolder2
+
+# folders/testfolder1 files:
+generatefile folders/testfolder1/file1.h
+generatefile folders/testfolder1/file2.h
+generatefile folders/testfolder1/file3.h
+generatefile folders/testfolder1/file4.h
+
+echo "OPA !!IGNORE-LINE!!\n" >> folders/testfolder1/fileignore1.h
+
+echo "OPA !!IGNORE-LINE!!\n" >> folders/testfolder1/file5.h
+generatefile folders/testfolder1/file5.h
+
+# folders/testfolder2 files:
+generatefile folders/testfolder2/file1.h
+generatefile folders/testfolder2/file2.h
+cp folders/testfolder1/file3.h folders/testfolder2/
+
+cp folders/testfolder1/fileignore1.h folders/testfolder2/
+sed -i 's/OPA/HEY/g' folders/testfolder2/fileignore1.h
+
+echo "HEY !!IGNORE-LINE!!\n" >> folders/testfolder2/file5.h
+generatefile folders/testfolder2/file5.h
---
+++ testscript.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo "Generating files..."
+./generaterandomfiles.sh
+
+echo -e "\n"
+echo "Calling updatefolder.py:"
+python updatefolder.py ./folders/testfolder1 ./folders/ *.h
+
+echo -e "\n"
+echo "Running Test verification:"
+cd folders/testfolder2/
+error=0
+for file in *; do
+	ret=$(diff -I '!!IGNORE-LINE!!' ../../folders/testfolder1/$file $file)
+	if [[ -n "${ret// }" ]]; then
+		echo -e "${RED}ERROR in $file ${NC}"
+		error=1
+	fi
+done
+cd $PWD
+if [ $error == 0 ]; then
+	echo -e "${GREEN}NO ERRORS FOUND!${NC}"
+else
+	exit 1
+fi
