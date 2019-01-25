6ca16ebf9e68b0e024a296ebabab7a3cdd3fe205
Caio Toledo <caio_toledo_2@hotmail.com>
2019-01-25 15:53:33 +0000

    Improve test script for Travis CI
    Signed-off-by: Caio Toledo <caio_toledo_2@hotmail.com>

--- testscript.sh
+++ testscript.sh
@@ -7,25 +7,25 @@ NC='\033[0m'
 echo "Generating files..."
 ./generaterandomfiles.sh
 
-echo -e "\n"
+echo "\n"
 echo "Calling updatefolder.py:"
 echo "python updatefolder.py ./folders/testfolder1 ./folders/ *.h"
-python updatefolder.py ./folders/testfolder1 ./folders/ *.h
+python updatefolder.py --FolderWithNewFiles ./folders/testfolder1 --FolderWithOldFiles ./folders/ --FilePattern *.h --IgnoreString '!!IGNORE-LINE!!' 'GENERATION TIME' 'GENERATED ON:'
 
-echo -e "\n"
+echo "\n"
 echo "Running Test verification:"
 cd folders/testfolder2/
 error=0
 for file in *; do
 	ret=$(diff -I '!!IGNORE-LINE!!' ../../folders/testfolder1/$file $file)
-	if [[ -n "${ret// }" ]]; then
-		echo -e "${RED}ERROR in $file ${NC}"
+	if [ ! -z "$ret" ]; then
+		echo "${RED}ERROR in $file ${NC}"
 		error=1
 	fi
 done
 cd $PWD
-if [ $error == 0 ]; then
-	echo -e "${GREEN}NO ERRORS FOUND!${NC}"
+if [ $error -eq 0 ]; then
+	echo "${GREEN}NO ERRORS FOUND!${NC}"
 else
 	exit 1
 fi
