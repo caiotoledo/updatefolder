47b8669c3d60945047ef209e8e75056b027e351c
Caio Toledo <caio_toledo_2@hotmail.com>
2019-02-16 23:02:45 +0000

    Add some comments in script testing
    Adding comments to show what each testing file intents to test.
    
    Signed-off-by: Caio Toledo <caio_toledo_2@hotmail.com>

--- generaterandomfiles.sh
+++ generaterandomfiles.sh
@@ -24,13 +24,17 @@ generatefile folders/testfolder1/file6.h
 # folders/testfolder2 files:
 generatefile folders/testfolder2/file1.h
 generatefile folders/testfolder2/file2.h
+# Create the same file to be ignore by updatefolder script
 cp folders/testfolder1/file3.h folders/testfolder2/
 
+# Create a file that has only a diff in the !!IGNORE-LINE!!, and should be ignored
 cp folders/testfolder1/fileignore1.h folders/testfolder2/
 sed -i 's/OPA/HEY/g' folders/testfolder2/fileignore1.h
 
+# Generate a file that has the !!IGNORE-LINE!! plus other differences, should be updated
 echo "HEY !!IGNORE-LINE!!\n" >> folders/testfolder2/file5.h
 generatefile folders/testfolder2/file5.h
 
+# Generate a file that has only decremental differences
 cp folders/testfolder1/file6.h folders/testfolder2/file6.h
 sed -i '$ d' folders/testfolder1/file6.h
